1e3bc6e80dca1c6389ca480575036705696c8d14
stoza <tcho_op@protonmail.com>
2024-02-14 23:42:23 +0100

    do minor modification

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,7 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = {'nvim-tree/nvim-web-devicons'}, 
     config = function()
+        --TODO should explore more the setup of lualine
         require('lualine').setup({ -- why exactly do we need the setup here??
             options = {
                 icons_enabled = true,
