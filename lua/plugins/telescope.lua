065eb896df2c97badc1b6f1c59527bfb258ca16c
stoza <tcho_op@protonmail.com>
2024-02-15 18:59:18 +0100

    add leadert to quickly see theme available and set it up

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,6 +8,6 @@ return {
         local builtin = require("telescope.builtin")
         vim.keymap.set('n', '<C-p>', builtin.find_files, {})
         vim.keymap.set('n', '<leader>p', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>t', builtin.colorscheme, {noremap = true})
     end
-
 }
