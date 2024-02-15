ee37fe7b489ddf643f671c9684a1bc2b4b874b25
stoza <tcho_op@protonmail.com>
2024-02-15 18:04:47 +0100

    added python LSP and change indent

--- lua/plugins/LSP.lua
+++ lua/plugins/LSP.lua
@@ -1,45 +1,49 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  -- mason-lspconfig
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup{
-        ensure_installed = {'clangd', 'gopls', 'jsonls', 'lua_ls', 'marksman'}, -- this allow to be sure the language we want are installed
-      }
-    end
-  },
-  -- nvim-lsp config (responsible for communication between nvim and lsp server)
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local LSPconfig = require('lspconfig')
-      local capabilities = require('cmp_nvim_lsp').default_capabilities() -- used to have the completion by lsp
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    -- mason-lspconfig
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup{
+                ensure_installed = {'clangd', 'gopls', 'jsonls', 'lua_ls', 'marksman', 'pyright'}, -- this allow to be sure the language we want are installed
+            }
+        end
+    },
+    -- nvim-lsp config (responsible for communication between nvim and lsp server)
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local LSPconfig = require('lspconfig')
+            local capabilities = require('cmp_nvim_lsp').default_capabilities() -- used to have the completion by lsp
 
-      --NOTE this is annoying because every setup needs to be done
-      -- mannullay. Maybe a way to circle on a variable
-      LSPconfig.lua_ls.setup({
-        capabilities = capabilities --used to setup the completion by the LSP
-      })
-      LSPconfig.clangd.setup({
-        capabilities = capabilities
-      })
-      LSPconfig.gopls.setup({
-        capabilities = capabilities
-      })
-      LSPconfig.jsonls.setup({
-        capabilities = capabilities
-      })
-      LSPconfig.marksman.setup({
-        capabilities = capabilities
-      })
-      -- keybindings (can find the list by typing :help vim.lsp.buf)
-      vim.keymap.set('n', 'R', vim.lsp.buf.rename, {}) -- MAJUSCULE R will rename all occurance of the word under cursor
-    end
-  }
+            --NOTE this is annoying because every setup needs to be done
+            -- mannullay. Maybe a way to circle on a variable
+            LSPconfig.lua_ls.setup({
+                capabilities = capabilities --used to setup the completion by the LSP
+            })
+            LSPconfig.clangd.setup({
+                capabilities = capabilities
+            })
+            LSPconfig.gopls.setup({
+                capabilities = capabilities
+            })
+            LSPconfig.jsonls.setup({
+                capabilities = capabilities
+            })
+            LSPconfig.marksman.setup({
+                capabilities = capabilities
+            })
+            LSPconfig.marksman.setup({
+                capabilities = capabilities
+            })
+
+            -- keybindings (can find the list by typing :help vim.lsp.buf)
+            vim.keymap.set('n', 'R', vim.lsp.buf.rename, {}) -- MAJUSCULE R will rename all occurance of the word under cursor
+        end
+    }
 }
