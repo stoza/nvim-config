2140fa937d12e3a00610d068a95db38d51cb65f0
stoza <tcho_op@protonmail.com>
2024-02-15 22:49:31 +0100

    update the keymap for the completion to something more logical

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -21,9 +21,9 @@ return {
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
-          ['<TAB>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ['<C-l>'] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select, count = 1}),
-          ['<C-h>'] = cmp.mapping.select_prev_item(),
+          ['<cr>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ['<TAB>'] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select, count = 1}),
+          ['<C-a>'] = cmp.mapping.select_prev_item(),
         }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
