bc61b634d0bcc2b71e47f85d9852f39106c72f6e
arnaud <a.stoz@cylab.be>
2024-03-06 09:08:55 +0100

    change the mapping for the completion to something more meaningfull

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -21,9 +21,9 @@ return {
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
-          ['<cr>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ['<TAB>'] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select, count = 1}),
-          ['<C-a>'] = cmp.mapping.select_prev_item(),
+          ['<TAB>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ['<C-j>'] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select, count = 1}),
+          ['<C-k>'] = cmp.mapping.select_prev_item(),
         }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
