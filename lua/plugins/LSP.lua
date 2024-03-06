d9f09b8ccfa7e375f4b1d6c7e1ea97f20783e0b8
arnaud <a.stoz@cylab.be>
2024-03-06 09:20:55 +0100

    add a raccourci for the reference but don't really like how it is display

--- lua/plugins/LSP.lua
+++ lua/plugins/LSP.lua
@@ -48,6 +48,7 @@ return {
             -- keybindings (can find the list by typing :help vim.lsp.buf)
             vim.keymap.set('n', 'R', vim.lsp.buf.rename, {}) -- MAJUSCULE R will rename all occurance of the word under cursor
             vim.keymap.set('n', '<leader>d', vim.lsp.buf.definition, {})
+            vim.keymap.set('n', '<leader>f', vim.lsp.buf.references, {})
         end
     }
 }
