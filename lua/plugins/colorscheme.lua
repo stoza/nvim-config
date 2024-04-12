3cb7ab4b06b5b754bab33ab858d83a054889c0ef
arnaud <a.stoz@cylab.be>
2024-04-12 11:18:07 +0200

    change theme to onedark

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,20 @@
 -- just uncomment the package you want to use
 return {
- -- {
- --   "catppuccin/nvim",
- --   name = "catppuccin",
- --   priority = 1000,
- --   config = function() -- Does not require setup anymore as it does it by default 
- --     vim.cmd.colorscheme "catppuccin"
- --   end
- -- },
-  {
-    "EdenEast/nightfox.nvim",
-    config = function ()
-      vim.cmd.colorscheme "nightfox"
-    end
-  }
+    -- {
+    --   "catppuccin/nvim",
+    --   name = "catppuccin",
+    --   priority = 1000,
+    --   config = function() -- Does not require setup anymore as it does it by default 
+    --     vim.cmd.colorscheme "catppuccin"
+    --   end
+    -- },
+    {
+        "EdenEast/nightfox.nvim",
+    },
+    {
+        "navarasu/onedark.nvim",
+        config = function ()
+            require('onedark').load()
+        end
+    }
 }
